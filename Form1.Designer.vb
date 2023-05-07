eb6680083356a029b53faffd230d538f1b8aa879
evanm <evanm@DESKTOP-BME31I7>
2023-05-07 18:53:49 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMeterCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMeterConversion = New System.Windows.Forms.Label()
+        Me.btnMeters = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lstMeters = New System.Windows.Forms.ListBox()
+        Me.lblSurvey = New System.Windows.Forms.Label()
+        Me.picCalculator = New System.Windows.Forms.PictureBox()
+        Me.grpConversions = New System.Windows.Forms.GroupBox()
+        Me.radCentimeterKilometer = New System.Windows.Forms.RadioButton()
+        Me.radCentimeterMeter = New System.Windows.Forms.RadioButton()
+        Me.radKilometerCentimeter = New System.Windows.Forms.RadioButton()
+        Me.radKilometerMeter = New System.Windows.Forms.RadioButton()
+        Me.radMeterCentimeter = New System.Windows.Forms.RadioButton()
+        Me.radMeterKilometer = New System.Windows.Forms.RadioButton()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picCalculator, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpConversions.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblMeterConversion
+        '
+        Me.lblMeterConversion.AutoSize = True
+        Me.lblMeterConversion.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblMeterConversion.Font = New System.Drawing.Font("Segoe UI Black", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMeterConversion.Location = New System.Drawing.Point(393, 543)
+        Me.lblMeterConversion.Name = "lblMeterConversion"
+        Me.lblMeterConversion.Size = New System.Drawing.Size(282, 32)
+        Me.lblMeterConversion.TabIndex = 10
+        Me.lblMeterConversion.Text = "XXXXXXXXXXXX 0000"
+        Me.lblMeterConversion.Visible = False
+        '
+        'btnMeters
+        '
+        Me.btnMeters.BackColor = System.Drawing.Color.LemonChiffon
+        Me.btnMeters.Font = New System.Drawing.Font("Segoe UI Black", 11.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMeters.Location = New System.Drawing.Point(537, 93)
+        Me.btnMeters.Name = "btnMeters"
+        Me.btnMeters.Size = New System.Drawing.Size(291, 51)
+        Me.btnMeters.TabIndex = 9
+        Me.btnMeters.Text = "Enter Meters"
+        Me.btnMeters.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.LemonChiffon
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(821, 400)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(156, 48)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'lstMeters
+        '
+        Me.lstMeters.Font = New System.Drawing.Font("Segoe UI Black", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstMeters.FormattingEnabled = True
+        Me.lstMeters.ItemHeight = 38
+        Me.lstMeters.Location = New System.Drawing.Point(845, 93)
+        Me.lstMeters.Name = "lstMeters"
+        Me.lstMeters.Size = New System.Drawing.Size(158, 232)
+        Me.lstMeters.TabIndex = 7
+        '
+        'lblSurvey
+        '
+        Me.lblSurvey.AutoSize = True
+        Me.lblSurvey.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblSurvey.Font = New System.Drawing.Font("Segoe UI Black", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSurvey.Location = New System.Drawing.Point(509, 21)
+        Me.lblSurvey.Name = "lblSurvey"
+        Me.lblSurvey.Size = New System.Drawing.Size(468, 45)
+        Me.lblSurvey.TabIndex = 6
+        Me.lblSurvey.Text = "Meter Conversion Caluclator"
+        '
+        'picCalculator
+        '
+        Me.picCalculator.BackgroundImage = Global.GUI_Final_Project.My.Resources.Resources.calc
+        Me.picCalculator.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.picCalculator.Location = New System.Drawing.Point(12, 21)
+        Me.picCalculator.Name = "picCalculator"
+        Me.picCalculator.Size = New System.Drawing.Size(488, 427)
+        Me.picCalculator.TabIndex = 11
+        Me.picCalculator.TabStop = False
+        '
+        'grpConversions
+        '
+        Me.grpConversions.Controls.Add(Me.radCentimeterKilometer)
+        Me.grpConversions.Controls.Add(Me.radCentimeterMeter)
+        Me.grpConversions.Controls.Add(Me.radKilometerCentimeter)
+        Me.grpConversions.Controls.Add(Me.radKilometerMeter)
+        Me.grpConversions.Controls.Add(Me.radMeterCentimeter)
+        Me.grpConversions.Controls.Add(Me.radMeterKilometer)
+        Me.grpConversions.Location = New System.Drawing.Point(590, 168)
+        Me.grpConversions.Name = "grpConversions"
+        Me.grpConversions.Size = New System.Drawing.Size(238, 220)
+        Me.grpConversions.TabIndex = 12
+        Me.grpConversions.TabStop = False
+        Me.grpConversions.Text = "Choose Conversion"
+        '
+        'radCentimeterKilometer
+        '
+        Me.radCentimeterKilometer.AutoSize = True
+        Me.radCentimeterKilometer.Location = New System.Drawing.Point(7, 176)
+        Me.radCentimeterKilometer.Name = "radCentimeterKilometer"
+        Me.radCentimeterKilometer.Size = New System.Drawing.Size(216, 24)
+        Me.radCentimeterKilometer.TabIndex = 5
+        Me.radCentimeterKilometer.TabStop = True
+        Me.radCentimeterKilometer.Text = "Centimeters to Kilometers"
+        Me.radCentimeterKilometer.UseVisualStyleBackColor = True
+        '
+        'radCentimeterMeter
+        '
+        Me.radCentimeterMeter.AutoSize = True
+        Me.radCentimeterMeter.Location = New System.Drawing.Point(7, 146)
+        Me.radCentimeterMeter.Name = "radCentimeterMeter"
+        Me.radCentimeterMeter.Size = New System.Drawing.Size(191, 24)
+        Me.radCentimeterMeter.TabIndex = 4
+        Me.radCentimeterMeter.TabStop = True
+        Me.radCentimeterMeter.Text = "Centimeters to Meters"
+        Me.radCentimeterMeter.UseVisualStyleBackColor = True
+        '
+        'radKilometerCentimeter
+        '
+        Me.radKilometerCentimeter.AutoSize = True
+        Me.radKilometerCentimeter.Location = New System.Drawing.Point(7, 116)
+        Me.radKilometerCentimeter.Name = "radKilometerCentimeter"
+        Me.radKilometerCentimeter.Size = New System.Drawing.Size(216, 24)
+        Me.radKilometerCentimeter.TabIndex = 3
+        Me.radKilometerCentimeter.TabStop = True
+        Me.radKilometerCentimeter.Text = "Kilometers to Centimeters"
+        Me.radKilometerCentimeter.UseVisualStyleBackColor = True
+        '
+        'radKilometerMeter
+        '
+        Me.radKilometerMeter.AutoSize = True
+        Me.radKilometerMeter.Location = New System.Drawing.Point(7, 86)
+        Me.radKilometerMeter.Name = "radKilometerMeter"
+        Me.radKilometerMeter.Size = New System.Drawing.Size(179, 24)
+        Me.radKilometerMeter.TabIndex = 2
+        Me.radKilometerMeter.TabStop = True
+        Me.radKilometerMeter.Text = "Kilometers to Meters"
+        Me.radKilometerMeter.UseVisualStyleBackColor = True
+        '
+        'radMeterCentimeter
+        '
+        Me.radMeterCentimeter.AutoSize = True
+        Me.radMeterCentimeter.Location = New System.Drawing.Point(7, 56)
+        Me.radMeterCentimeter.Name = "radMeterCentimeter"
+        Me.radMeterCentimeter.Size = New System.Drawing.Size(191, 24)
+        Me.radMeterCentimeter.TabIndex = 1
+        Me.radMeterCentimeter.TabStop = True
+        Me.radMeterCentimeter.Text = "Meters to Centimeters"
+        Me.radMeterCentimeter.UseVisualStyleBackColor = True
+        '
+        'radMeterKilometer
+        '
+        Me.radMeterKilometer.AutoSize = True
+        Me.radMeterKilometer.Location = New System.Drawing.Point(7, 26)
+        Me.radMeterKilometer.Name = "radMeterKilometer"
+        Me.radMeterKilometer.Size = New System.Drawing.Size(179, 24)
+        Me.radMeterKilometer.TabIndex = 0
+        Me.radMeterKilometer.TabStop = True
+        Me.radMeterKilometer.Text = "Meters to Kilometers"
+        Me.radMeterKilometer.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.LemonChiffon
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(860, 544)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(117, 35)
+        Me.btnExit.TabIndex = 13
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'frmMeterCalculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(1015, 627)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.grpConversions)
+        Me.Controls.Add(Me.picCalculator)
+        Me.Controls.Add(Me.lblMeterConversion)
+        Me.Controls.Add(Me.btnMeters)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstMeters)
+        Me.Controls.Add(Me.lblSurvey)
+        Me.Name = "frmMeterCalculator"
+        Me.Text = "Meter Calculator"
+        CType(Me.picCalculator, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpConversions.ResumeLayout(False)
+        Me.grpConversions.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblMeterConversion As Label
+    Friend WithEvents btnMeters As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lstMeters As ListBox
+    Friend WithEvents lblSurvey As Label
+    Friend WithEvents picCalculator As PictureBox
+    Friend WithEvents grpConversions As GroupBox
+    Friend WithEvents radCentimeterKilometer As RadioButton
+    Friend WithEvents radCentimeterMeter As RadioButton
+    Friend WithEvents radKilometerCentimeter As RadioButton
+    Friend WithEvents radKilometerMeter As RadioButton
+    Friend WithEvents radMeterCentimeter As RadioButton
+    Friend WithEvents radMeterKilometer As RadioButton
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class frmMeterCalculator
+    Private Sub frmMeterCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        radMeterKilometer.Select()
+    End Sub
+    Private Sub btnMeters_Click(sender As Object, e As EventArgs) Handles btnMeters.Click
+        Dim strMeter As String
+        Dim decMeter As Decimal
+        Dim decSumOfMeters As Decimal
+        Dim decConversion As Decimal = 0D
+
+        Dim strIBoxMsg As String = "Enter the number of Meters you would like to convert "
+        Dim strIBoxTitle As String = "Meter Conversion"
+
+        Dim intMaxEntries As Integer = 10
+        Dim intEntries As Integer = 1
+
+        strMeter = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+
+        Do Until intEntries > intMaxEntries Or strMeter = ""
+            If IsNumeric(strMeter) Then
+                decMeter = Convert.ToDecimal(strMeter)
+                If decMeter > 0 Then
+                    lstMeters.Items.Add(decMeter)
+                    decSumOfMeters += decMeter
+                    intEntries += 1
+                    strIBoxMsg = strIBoxMsg
+                Else
+                    MsgBox("Enter a positive number", , "Invalid Number Error")
+                End If
+                If intEntries <= intMaxEntries Then
+                    strMeter = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+                End If
+            Else
+                MsgBox("Enter a Number", , "Invalid Input Error")
+                strMeter = InputBox(strIBoxMsg & intEntries, strIBoxTitle)
+            End If
+        Loop
+
+        lblMeterConversion.Visible = True
+
+        If intEntries > 1 Then
+            ConversionCalculate(decConversion, decSumOfMeters)
+
+        Else
+            lblMeterConversion.Text = "No Meter Values Entered"
+        End If
+
+        btnMeters.Enabled = False
+    End Sub
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lstMeters.Items.Clear()
+        lblMeterConversion.Text = ""
+        btnMeters.Enabled = True
+        radMeterKilometer.Select()
+    End Sub
+
+    Private Sub ConversionCalculate(ByVal decConversion As Decimal, ByVal decSumOfMeters As Decimal)
+        If radMeterKilometer.Checked Then
+            decConversion = decSumOfMeters * 0.001
+            lblMeterConversion.Text = decSumOfMeters & " Meters is " & decConversion.ToString("F2") & " Kilometers "
+        ElseIf radMeterCentimeter.Checked Then
+            decConversion = decSumOfMeters * 100
+            lblMeterConversion.Text = "Meters to Centimeters " & decConversion.ToString("F2") & " Centimeters "
+        ElseIf radKilometerMeter.Checked Then
+            decConversion = decSumOfMeters * 1000
+            lblMeterConversion.Text = decSumOfMeters & " Kilometers is " & decConversion.ToString("F2") & " Meters "
+        ElseIf radKilometerCentimeter.Checked Then
+            decConversion = decSumOfMeters * 100000
+            lblMeterConversion.Text = decSumOfMeters & " Kilometers is " & decConversion.ToString("F2") & " Centimeters "
+        ElseIf radCentimeterMeter.Checked Then
+            decConversion = decSumOfMeters * 0.01
+            lblMeterConversion.Text = decSumOfMeters & " Centimeters is " & decConversion.ToString("F5") & " Meters "
+        ElseIf radCentimeterKilometer.Checked Then
+            decConversion = decSumOfMeters * 0.00001
+            lblMeterConversion.Text = decSumOfMeters & " Centimeters is " & decConversion.ToString("F5") & " Kilometers "
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
